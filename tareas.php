bac4af09152af2c9915b06f2805b150923e1e1eb
aarias2022 <201804329h@utea.edu.pe>
2022-06-30 21:33:06 -0500

    introduccion de Js para la creacion de los botones de estado

--- tareas.php
+++ tareas.php
@@ -74,5 +74,6 @@ if(!isset($_SESSION['valid'])) {
 </table>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
